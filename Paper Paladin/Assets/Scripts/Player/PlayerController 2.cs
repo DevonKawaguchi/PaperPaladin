4061fdb52d4580859af1d549697189abcb044ab6
DevonKawaguchi <DKawaguchi@student.saintaug.nsw.edu.au>
2024-04-15 21:27:56 +1000

    Tile-Based Movement, Player Animations, Object Collisions, and Enemy Encounters Base Logic
    Implemented code logic for basic tile-based movement, for stopping player movement on certain objects, and for enemy encounters, as well as created the basic structure for the game world through tilemaps and tile palettes and blend trees to create idle and walking player movement animations.
    
    Encountered minimal challenges as I followed Game Dev Experiement's "Make a Game Like Pokemon in Unity" YouTube series, though had to troublshoot my art sprites for a while as I transferred the project to a new computer and some of the sprites got corrupted. As a preface, I will be following the YouTube series to setup the basic mechanics of the game as I don't have significant experience in Unity, though I will be heavily modifying and revising it once I've implemented the necessary features.

--- Paper Paladin/Assets/Scripts/Player/PlayerController 2.cs
+++ Paper Paladin/Assets/Scripts/Player/PlayerController 2.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public LayerMask solidObjectsLayer;
+    public LayerMask grassLayer;
 
     private bool isMoving;
     private Vector2 input;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -16,17 +25,23 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
-            if (input.x != 0) input.y = 0; // removes diagonal movement
+            if (input.x != 0) input.y = 0; //Removes diagonal movement
 
-            if (input != Vector2.zero)
+            if (input != Vector2.zero) //Allows player to remain in previous animation, e.g: will remain in "idle_left" animation state if last input was left - this is because the if statement only changes "MoveX"/"MoveY" float parameters if input is not zero.
             {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
-                StartCoroutine(Move(targetPos));
+                if (IsWalkable(targetPos)) //Stops player from moving if they are walking over tiles labelled "SolidObjects"
+                    StartCoroutine(Move(targetPos));
             }
         }
+
+        animator.SetBool("isMoving", isMoving);
     }
 
     IEnumerator Move(Vector3 targetPos)
@@ -41,5 +56,30 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+
+        CheckForEncounters();
+    }
+
+    private bool IsWalkable(Vector3 targetPos) //Determines if player is walking over tiles labelled "SolidObjects"
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void CheckForEncounters()
+    {
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, grassLayer) != null)
+        {
+            if (Random.Range(1, 101) <= 10)
+            {
+                Debug.Log("Encountered a wild pokemon"); //Temporary code
+            }
+        }
     }
 }
+
+//Player Controller Position Note: Make sure x position value is in the format x.5 and y position value in the format x.8. y position value format can also be the default x.5, though x.8 is more visually appealing as it allows the player model to be slightly above the centre of the tiles
